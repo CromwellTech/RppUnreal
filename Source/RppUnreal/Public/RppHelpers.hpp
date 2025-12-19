e2045646eb36c917b65561a88dde8f4d9806d0c0
Simon Cromwell <cromwellsimon@gmail.com>
2025-12-18 22:47:16 -0500

    Rename RppHelpers.h to RppHelpers.hpp

--- Source/RppUnreal/Public/RppHelpers.hpp
+++ Source/RppUnreal/Public/RppHelpers.hpp
@@ -1,14 +1,7 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
-
-#pragma once
-
-#include "CoreMinimal.h"
+﻿#pragma once
 #include "observables/observable.hpp"
 
-/**
- * 
- */
-class RPPUNREAL_API RppHelpers
+class RppHelpers
 {
 public:
 	static auto TakeWhileNotNull(auto* Self)
